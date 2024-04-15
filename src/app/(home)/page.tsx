98209b5930ed1874902b4871fcf11d2960d0ced9
abhik199 <abhishekkirar2004@gmail.com>
2024-04-15 06:03:30 +0530

    addd here

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,26 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
-  return <Button>Button</Button>;
+  return (
+    <>
+      <section className=" bg-white">
+        <div className="container flex items-center justify-between py-24">
+          <div>
+            <h1 className=" text-7xl font-black font-sans leading-2" >Super Delicious Pizza in <br />
+              <span className=" text-primary" >Only 45 minutes</span>
+            </h1>
+            <p className="text-2xl mt-8 max-w-lg leading-snug">Enjoy a free Meal If your Order Takes More Than 45 minutes</p>
+            <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold" >
+              Get your pizza now
+            </Button>
+
+          </div>
+          <div>
+            <Image alt="pizza-img" src={"/pizza-margherita.png"} width={400} height={400} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
